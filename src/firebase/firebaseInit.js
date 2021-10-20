fde9c8befe0c7134318a3b2584819555b073f42a
= <richardoehbet221@gmail.com>
2021-10-20 08:38:31 +0700

    Update firebase config

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app'
+import 'firebase/firestore'
+
 const firebaseConfig = {
     apiKey: "AIzaSyBBlOTZ_DrOi7HbywtfH4KcqYFZIIO7zV8",
     authDomain: "fireblogs-6f7a5.firebaseapp.com",
@@ -8,4 +10,10 @@ const firebaseConfig = {
     appId: "1:48699153852:web:2e0d5143982b7abde21fb9",
     measurementId: "G-3HMB6M9Q0G"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const timeStamp = firebase.firestore.FieldValue.serverTimestamp
+
+export {timeStamp}
+export default firebaseApp.firestore()
   
