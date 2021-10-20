f429606c849bbeeefb7b4cf13feb914e6faee594
= <richardoehbet221@gmail.com>
2021-10-20 08:11:27 +0700

    Test push

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+const firebaseConfig = {
+    apiKey: "AIzaSyBBlOTZ_DrOi7HbywtfH4KcqYFZIIO7zV8",
+    authDomain: "fireblogs-6f7a5.firebaseapp.com",
+    projectId: "fireblogs-6f7a5",
+    storageBucket: "fireblogs-6f7a5.appspot.com",
+    messagingSenderId: "48699153852",
+    appId: "1:48699153852:web:2e0d5143982b7abde21fb9",
+    measurementId: "G-3HMB6M9Q0G"
+  };
+  
