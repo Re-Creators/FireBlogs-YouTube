e7043df4278d4b837ce50cfcce9abaf68229606c
= <richardoehbet221@gmail.com>
2021-10-21 20:40:29 +0700

    Update file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,31 @@
   <div class="home">
     <blog-post :post="welcomeScreen"/>
     <blog-post :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+    <div class="blog-card-wrap">
+      <div class="container">
+          <h3>View More Recent Blogs</h3>
+          <div class="blog-cards">
+            <blog-card :post="post" v-for="(post, index) in sampleBlogCard" :key="index" />
+          </div>
+      </div>
+    </div>
+    <div class="updates">
+      <div class="container">
+        <h2>never miss a post. Register for your  free account today!</h2>
+        <router-link to="#" class="router-button">Register for FireBlogs <Arrow class="arrow arrow-light"/></router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BlogCard from '../components/BlogCard.vue';
 import BlogPost from '../components/BlogPost.vue';
+import Arrow from "../assets/Icons/arrow-right-light.svg";
 
 export default {
   name: "Home",
-  components: {BlogPost},
+  components: {BlogPost, BlogCard, Arrow},
   data() {
     return {
       welcomeScreen: {
@@ -31,8 +47,59 @@ export default {
           blogHTML : 'This is a filter blog post',
           blogCoverPhoto : 'designed-for-everyone',
         }
-      ]
+      ],
+      
+    }
+  },
+  computed: {
+    sampleBlogCard() {
+      return this.$store.state.sampleBlogCard
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media(min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media(min-width: 800px){
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      text-align: center;
+      text-transform: uppercase;
+      @media(min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
+  }
+}
+</style>
